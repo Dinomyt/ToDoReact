1dfb859a41238072253fe3b46ac572fad5d2dbb2
Dinomyt <149448460+Dinomyt@users.noreply.github.com>
2024-05-21 20:01:59 -0700

    +finalized components

---
+++ to-do-react - Copy/src/App.tsx
@@ -0,0 +1,11 @@
+import Todo from "./Components/ToDo/Todo"
+
+const App = () => {
+  return (
+    <>
+      <Todo/>
+    </>
+  )
+}
+
+export default App
--- to-do-react/src/Components/ToDo/Todo.tsx
+++ to-do-react/src/Components/ToDo/Todo.tsx
@@ -4,11 +4,14 @@ import { useState, ChangeEvent } from 'react';
 interface TodoItem {
   id: number;
   todo: string;
+  completed: boolean;
 }
 
 const Todo = () => {
   const [input, setInput] = useState('');
   const [list, setList] = useState<TodoItem[]>([]);
+  const [editValue, setEditValue] = useState('');
+  const [editId, setEditId] = useState<number | null>(null);
 
   const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
     setInput(event.target.value);
@@ -19,7 +22,8 @@ const Todo = () => {
 
     const newTodo: TodoItem = {
       id: Math.random(),
-      todo: input
+      todo: input,
+      completed: false
     };
 
     setList([...list, newTodo]);
@@ -28,6 +32,29 @@ const Todo = () => {
 
   const removeTodo = (id: number) => {
     setList(list.filter(item => item.id !== id));
+  };
+
+  const editTodo = (id: number, edit: string) => {
+    setList(list.map(item => item.id === id ? {...item, todo: edit} : item));
+  };
+
+  const startEditing = (item: TodoItem) => {
+    setEditId(item.id);
+    setEditValue(item.todo);
+  };
+
+  const saveEdit = (id: number) => {
+    editTodo(id, editValue);
+    setEditId(null);
+    setEditValue('');
+  }
+
+  const handleEditChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setEditValue(event.target.value);
+  };
+
+  const toggleComplete = (id: number) => {
+    setList(list.map(item => item.id === id ? { ...item, completed: !item.completed } : item));
   };
 
   return (
@@ -42,15 +69,38 @@ const Todo = () => {
           <thead>
             <tr>
             <th style={{ textAlign: 'left' }}>To do</th>
-              <th>Action</th>
             </tr>
           </thead>
           <tbody>
             {list.map((item) => (
               <tr key={item.id}>
-                <td>{item.todo}</td>
+                <td style={{ textDecoration: item.completed ? 'line-through' : 'none' }}>
+                  {editId === item.id ? (
+                  <input
+                    type="text"
+                    value={editValue}
+                    onChange={handleEditChange}
+                  />
+                ) : ( 
+                  item.todo
+                )}</td>
                 <td>
-                  <button onClick={() => removeTodo(item.id)} className={Styles.removeButton}>Remove</button>
+                  {editId === item.id ? (
+                    <button onClick={() => saveEdit(item.id)} className={Styles.saveButton}>Save</button>
+                  ) : (
+                    <>
+                    <input
+                        type="checkbox"
+                        checked={item.completed}
+                        onChange={() => toggleComplete(item.id)}
+                      />
+
+                      <button onClick={() => removeTodo(item.id)} className={Styles.removeButton}>Remove</button>
+                      <button onClick={() => startEditing(item)} className={Styles.removeButton}>Edit</button>
+                      
+                    </>
+                  )}
+
                 </td>
               </tr>
             ))}
